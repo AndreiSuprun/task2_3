7c2b3f0a695233e3809cd748d69e0cac6aef3aa9
AndreiSuprun <66207171+AndreiSuprun@users.noreply.github.com>
2020-10-05 00:00:52 +0300

    Delete PeriodicalCategoryDao.java

--- main/java/com/suprun/periodicals/dao/PeriodicalCategoryDao.java
+++
@@ -1,6 +0,0 @@
-package com.suprun.periodicals.dao;
-
-import com.suprun.periodicals.entity.PeriodicalCategory;
-
-public interface PeriodicalCategoryDao extends GenericDao<PeriodicalCategory, Integer> {
-}
