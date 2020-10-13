4bbd8bdf562f6fd0378832092c82fefcfb94338d
AndreiSuprun <66207171+AndreiSuprun@users.noreply.github.com>
2020-10-13 15:48:29 +0300

    Delete SubscriptionPeriodDao.java

--- java/com/suprun/periodicals/dao/SubscriptionPeriodDao.java
+++
@@ -1,6 +0,0 @@
-package com.suprun.periodicals.dao;
-
-import com.suprun.periodicals.entity.SubscriptionPeriod;
-
-public interface SubscriptionPeriodDao extends GenericDao<SubscriptionPeriod, Integer> {
-}
