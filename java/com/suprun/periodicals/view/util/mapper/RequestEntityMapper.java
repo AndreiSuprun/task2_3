b2e66cb0e872334f10b5be2fcb22b6641495273d
AndreiSuprun <66207171+AndreiSuprun@users.noreply.github.com>
2020-10-13 15:54:24 +0300

    Delete RequestEntityMapper.java

--- java/com/suprun/periodicals/view/util/mapper/RequestEntityMapper.java
+++
@@ -1,8 +0,0 @@
-package com.suprun.periodicals.view.util.mapper;
-
-import javax.servlet.http.HttpServletRequest;
-
-public interface RequestEntityMapper<T> {
-
-    T mapToObject(HttpServletRequest request);
-}
